6aafad153a836a7082d053561f1a89d2301c580e
vinniiotchkov <vinniiotchkov@gmail.com>
2017-10-03 23:24:57 -0700

    Quick bug fix

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { Text } from 'react-native';
 import { connect } from 'react-redux';
 
 class PostList extends Component {
   render() {
     console.log('THIS.PROPS', this.props);
     console.log('THIS.PROPS.POSTS', this.props.posts);
-    return;
+    return(
+      <Text></Text>
+    );
   }
 }
 
